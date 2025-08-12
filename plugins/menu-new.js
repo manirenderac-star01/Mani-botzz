6827ff33badef9fd7d22ff409cd3ee4661125142
manibot375 <maharsahil5417@gmail.com>
2025-08-12 11:30:42 +0500

    Update menu-new.js

--- plugins/menu-new.js
+++ plugins/menu-new.js
@@ -39,7 +39,7 @@ cmd({
             forwardingScore: 999,
             isForwarded: true,
             forwardedNewsletterMessageInfo: {
-                newsletterJid: '120363348739987203@newsletter',
+                newsletterJid: '120363405446516046@g.us',
                 newsletterName: config.OWNER_NAME,
                 serverMessageId: 143
             }
